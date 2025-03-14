4f3d22aa525c46813dfb524b468399b7e6dae732
giusmili <giusmili67@gmail.com>
2025-03-14 10:15:54 +0100

    php

---
+++ model/data.php
@@ -0,0 +1,31 @@
+<?php
+    const data = [
+        [
+            "cover","?"
+            "title"=>"King Kong",
+            "date" => "1933",
+            "real" => "Merian C.Cooper, Ernest B",
+            "genre" =>"Horreur, fantastique",
+            "sortie DVD" =>"22 novembre 2005",
+            "url"=>"link"
+        ], [
+            "cover","?"
+            "title"=>"Scarface",
+            "date" => "1932",
+            "real" => "Howard Hawks",
+            "genre" =>"Policier, action",
+            "sortie" =>"Dvd : 23 décembre 2005",
+            "url"=>"link"
+            
+        ],
+        [
+            "cover","?"
+            "title"=>"Autant en emporte le vent",
+            "date" => "1939",
+            "real" => "Victor Fleming",
+            "genre" =>"Dramatique, guerre",
+            "sortie" =>"Dvd : 19 mars 2005",
+            "url"=>"link"
+            
+        ],
+    ]
